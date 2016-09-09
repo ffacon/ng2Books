60907464d6d136b2daffb160252f8eccdee2e558
ffacon <francois.facon@gmail.com>
2016-09-09 17:18:38 +0200

    PW7 Forms

--- app/src/components/contact/contact.ts
+++ app/src/components/contact/contact.ts
@@ -1,7 +1,20 @@
 import {Component} from '@angular/core';
+import {FormBuilder, ControlGroup, Control, Validators} from '@angular/common';
 
 import {ContactService} from '../../services/contactService';
 
+
+function containsValidCharacters(c:Control){
+
+	let specialChars= ['\\','<', '>', '&' ];
+  
+	for (let i in specialChars){
+		if ( c.value !== undefined && c.value.indexOf(specialChars[i]) != -1 ){
+		  return {'containsValidCharacters': true };
+		}
+	}
+	return null;
+}
 
 
 @Component({
@@ -17,12 +30,22 @@ export class Contact{
 
 	message: string= "";
 
-	constructor(contactService: ContactService){
+	_controlGroup: ControlGroup;
+
+	constructor(contactService: ContactService,  fb: FormBuilder){
 
 		this.email= contactService.email;
 		this.phone= contactService.phone;
 		this.address= contactService.address;
+		
+		this._controlGroup= fb.group({
+			'message': new Control('', Validators.compose([Validators.required, containsValidCharacters]))
+		});
 
+	}
+
+	get controlGroup(){
+		return this._controlGroup;
 	}
 
 	updateMessage(data: any){
